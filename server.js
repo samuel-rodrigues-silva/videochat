7f118439ae0d06aa6eb5de1e967f1f31e6d815df
Samuel Silva <samuel-rodrigues-silva>
2021-07-07 17:31:36 -0300

    refactor: changing configurations to respond client

--- server.js
+++ server.js
@@ -2,35 +2,32 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-const { v4: uuidV4 } = require('uuid');
+const io = require('socket.io')(server, {
+    cors: {
+        origins: ['http://localhost:4200']
+    }
+});
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, '/views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`);
-})
-
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.params.room })
-})
+app.use((request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 io.on("connection", socket => {
     socket.on('join-room', (roomId, userId) => {
         console.log('ROOMID:' + roomId);
         console.log('USERID:' + userId);
         socket.join(roomId);
-        socket.to(roomId);
-        socket.broadcast.emit('user-connected', userId);
-
+        socket.to(roomId).emit('user-connected', userId);
 
         socket.on('disconnect', () => {
-            socket.to(roomId);
-            socket.broadcast.emit('user-disconnected', userId);
+            socket.to(roomId).emit('user-disconnected', userId);
         })
+    })
+
+    socket.on('connect-error', error => {
+        console.log(error)
     })
 })
 
